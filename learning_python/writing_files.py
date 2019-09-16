83065bd29d6b7a4f7af4c456b24ff8eb0a5d6e09
srisaipog <sridhar.sairam21@ycdsbk12.ca>
2019-09-16 10:54:44 -0400

    More file management

--- learning_python/writing_files.py
+++ learning_python/writing_files.py
@@ -8,6 +8,36 @@ with open("/home/robuntu/Sairam/ICS4U-Classwork/learning_python/Tell me what u w
 with open("/home/robuntu/Sairam/ICS4U-Classwork/learning_python/Tell me what u want.txt", "r") as f:
     bob = f.read()
 
+f = open("bob.txt", "w")
+f.write("Manually opening and closing. the 'with' automatically closes")
+f.close()
 blip = os.path.abspath("writing_files.py")
+
+# Converting data
+# data must be written as a string
+# and must be converted back to the desired
+# datatype once read.
+with open("some_file.txt", 'w') as f:
+    f.write(str(100))
+
+with open("some_file.txt", 'r') as f:
+    value = int(f.read())
+
+print(value * 2)
+
+# Create 100 files called file{num}.txt
+# put "hello" in each file
+for n in range(100):
+    with open(f"file{n}.txt", 'w') as f:
+        f.write("Hello")
+
+# Remove all files in the directory that start with 'file'
+import os
+
+file_list = os.listdir()
+
+for name in file_list:
+    if name.startswith('file'):
+        os.remove(name)
 
 print(blip)
