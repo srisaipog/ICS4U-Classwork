95bc30c3885818bff57c30e4db445ede9fb984ea
srisaipog <sridhar.sairam21@ycdsbk12.ca>
2019-10-02 10:52:08 -0400

    Added homework questions

---
+++ function_testing_practice.py
@@ -0,0 +1,24 @@
+# Write function headers and docstring for the following:
+
+# sum
+
+# math.ceil
+
+# math.floor
+
+# Function that takes a name and outputs a message
+
+# Function that takes two floats and funcs their average
+
+# Function that takes an age and returns True if they are voting age
+
+
+"""
+
+Make sure to make each function with proper:
+- Format
+- Documentation
+- Tests
+- Actual code
+
+"""
