f018c6494b0964852fcde934a374b6012e97c982
srisaipog <sridhar.sairam21@ycdsbk12.ca>
2019-10-02 10:45:04 -0400

    Added Proper function notation file

---
+++ function_notation.py
@@ -0,0 +1,30 @@
+
+# Generic function: how to write
+
+# For using List as a parameter:
+# from typing import List
+
+
+def function_name(param1: type, param2: type) -> return_type:
+    
+    """Sort out description of the function
+
+    Args:
+        param1: Explain the first parameter.
+        param2: Explain the second parameter.
+    Returns:
+        Explain what the function returns
+    """
+
+    pass
+
+
+def function_nom(parameter_1: type, parameter_2: type) -> return_type:
+    '''Sort out the description of the function
+
+    Arguments
+        parameter_1: Explain what parameter_1 is/does
+        parameter_2: Explain what parameter_2 is/does
+    Returns:
+        Explain what the function returns/does
+    '''
