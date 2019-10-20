5ec0b8dd0094b1c566af4d005b59c738afef0feb
srisaipog <43004256+srisaipog@users.noreply.github.com>
2019-10-20 13:23:44 -0400

    Update Classes Quiz Prep.py

--- Assessment Practice/Classes_Test_Prep/Classes Quiz Prep.py
+++ Assessment Practice/Classes_Test_Prep/Classes Quiz Prep.py
@@ -25,7 +25,23 @@ nutrition: int
 """
 
 class Food:
+    """
+    Attrs:
+        name (str): name of the food
+        cost (int): cost of the food in dollars
+        nutrution (int): the amount of nutrution the food gives
+    """
+
     def __init__(self, name: str, cost: int, nutrition: int):
+        """Makes a food object
+        Args:
+            self: self
+            name (str): the name of the food
+            cost (int): the cost of the food in dollars
+            nutrution (int): the amound of nutrition the food gives
+        Returns:
+            None
+        """
         self.name = name
         self.cost = cost
         self.nutrition = nutrition
@@ -46,26 +62,69 @@ bark() -> void
 """
 
 class Dog:
+    """
+    Attrs:
+        breed (str): The breed of the dog
+        name (str): The name of the dog
+        happiness (int): The happiness level of the dog
+    """
     def __init__(self, breed: str, name: str, happiness: int):
+        """Makes a dog object
+        Args:
+            self: self
+            breed (str): The breed of the dog
+            name (str): The name of the dog
+            happiness (int): The happiness level of the dog
+        Returns:
+            None
+        """
         self.breed = breed
         self.name = name
         self.happiness = happiness
 
     
     def __str__(self):
+        """Tells the name of the dog and its happiness level
+        Args:
+            self: self
+        Returns:
+            A string with the dog name and happiness level
+        """
+
         return f"{self.name} has {self.happiness} happiness"
 
 
     def eat(self, food):
+        """Makes the dog eat (increases happiness)
+        Args:
+            self: self
+            food: food object
+        Returns:
+            None
+        """
         self.happiness += 0.1 * food.nutrition
 
     
     def bark(self, times: int = 1):
+        """Makes the dog bark
+        Args:
+            self: self
+            times (int): Number of times dog should bark
+        Returns:
+            None
+        """
         for i in range(times):
             print("RUFF RUFF!")
 
 
 def main():
+    """Tests out Dog and Food Classes
+    Args:
+        None
+    Returns:
+        None
+    """
+    
     klondike = Dog("Toy American Eskimo", "Klondike", 720)
     sundae = Dog("Toy American Eskimo", "Sundae", 230)
     kermit = Dog("Italian Greyhound", "Kermit", 5)
