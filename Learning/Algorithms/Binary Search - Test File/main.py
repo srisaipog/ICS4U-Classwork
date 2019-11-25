0750537fd14fc593c7c59bb084f2918a713856fc
Sridhar Sairam <sridhar.sairam21@ycdsbk12.ca>
2019-11-25 10:00:10 -0500

    Added Test Files

---
+++ Learning/Algorithms/Binary Search - Test File/main.py
@@ -0,0 +1,18 @@
+"""
+Create a function with the following details:
+binary_search(target: int, numbers: int) -> int
+
+It will return the index location, not the found value. 
+Return -1 if the value is not in the list.
+"""
+
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    os.system("pytest")  # comment out if needed
+    main()
