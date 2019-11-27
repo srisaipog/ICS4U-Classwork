a515acd7696fdb6f3ca6bbfb0d03cf3c5a10ef4d
Sridhar Sairam <sridharsairam@Sris-PC.local>
2019-11-26 22:28:49 -0500

    Finished linear vs binary

--- Learning/Algorithms/Binary Search - Test File/main.py
+++ Learning/Algorithms/Binary Search - Test File/main.py
@@ -7,12 +7,12 @@ Return -1 if the value is not in the list.
 """
 
 import os
-
+from typing import List
 
 def main():
     pass
 
-
+'''
 def binary_search(target, data):
     first = 0
     last = len(data) - 1
@@ -32,10 +32,32 @@ def binary_search(target, data):
         elif data[mid] > target:
             last = mid
             mid = (start + last) // 2
+'''
 
+
+def binary_search(find: int, numbers: List[int]) -> int:
+    first = 0
+    last = len(numbers) - 1
+    mid = last + first // 2
+
+    while True:
+
+        not_found = (last - first) <= 0
+
+        if not_found:
+            return -1
+        
+        if numbers[mid] == find:
+            return mid
+        elif numbers[mid] > find:
+            last = mid
+            mid = (last + first) // 2
+        elif numbers[mid] < find:
+            first = mid
+            mid = (last + first) // 2
 
 
 if __name__ == "__main__":
-    # os.system("pytest")  # comment out if needed
+    os.system("pytest")  # comment out if needed
     main()
 
--- Learning/Algorithms/Binary VS Linear/main.py
+++ Learning/Algorithms/Binary VS Linear/main.py
@@ -15,35 +15,40 @@ def linear_search(target: int, data: List) -> int:
     return -1
 
 
-def binary_search(target: int, data: List) -> int:
-    start = 0
-    end = len(data) -1
-    mid = (start + end) // 2
+def binary_search(find: int, numbers: List[int]) -> int:
+    first = 0
+    last = len(numbers) - 1
+    mid = last + first // 2
 
     while True:
-        if data[mid] == target:
-            return mid
-        elif data[mid] > target:
-            end = mid
-            mid = (start + end) // 2
-        elif data[mid] < target:
-            start = mid
-            mid = (start + end) // 2
-        
-        if end - start == 1:
+
+        not_found = (last - first) <= 0
+
+        if not_found:
             return -1
+        
+        if numbers[mid] == find:
+            return mid
+        elif numbers[mid] > find:
+            last = mid
+            mid = (last + first) // 2
+        elif numbers[mid] < find:
+            first = mid
+            mid = (last + first) // 2
 
 
 for i in range(1, 12):
     DATASET_SIZE = 10**i
-    NUMBER = 1
+    NUMBER = 10
     SEARCH_TARGET = DATASET_SIZE - 1
-    
+    # SEARCH_TARGET = 0
+    # SEARCH_TARGET = DATASET_SIZE // 2
+
     # dataset = list(range(DATASET_SIZE))
-    dataset = (range(DATASET_SIZE))
+    dataset = list(range(DATASET_SIZE))
 
     result = timeit.timeit(
-        "linear_search(SEARCH_TARGET, dataset)",
+        "binary_search(SEARCH_TARGET, dataset)",
         number=NUMBER,
         globals=globals())
 
