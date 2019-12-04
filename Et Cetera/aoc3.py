adee6755e70a87b6c335ee96e179ec586fb5ad81
srisaipog <43004256+srisaipog@users.noreply.github.com>
2019-12-04 07:12:31 -0500

    Create aoc3.py

---
+++ Et Cetera/aoc3.py
@@ -0,0 +1,16 @@
+# functions!
+# 1. Manhattan Distance (p1: Tuple, p2: Tuple) -> int
+# 2. get route of path (p1: Tuple, direction: str) -> List[Tuple]
+# trial and error to see which manhattno distance is closest
+
+'''
+vars
+
+wire_1 = [Tuple]
+wire_2 = [Tuple]
+
+tangles = [Tuple]
+
+origin = (1, 1)
+
+'''
